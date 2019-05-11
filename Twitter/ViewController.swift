861ab79a0a3b4e5a68622d6f5bd02b6360580f14
Phuc Nguyen <ntp201094@gmail.com>
2019-05-11 11:28:09 +0700

    Initial Commit

---
+++ Twitter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Twitter
+//
+//  Created by Phuc Nguyen on 5/11/19.
+//  Copyright © 2019 Phuc Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
